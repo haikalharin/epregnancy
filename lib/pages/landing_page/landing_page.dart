428e7dc2df66a958337f7a785f34a126811034ca
HaikalHarin <haikal.harin@allianz.co.id>
2022-06-17 14:21:02 +0700

    fix landing page

--- lib/pages/landing_page/landing_page.dart
+++ lib/pages/landing_page/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:PregnancyApp/pages/landing_page/slider_modal.dart';
+import 'package:PregnancyApp/pages/landing_page/widget/slider_list.dart';
 import 'package:flutter/material.dart';
 
 import '../example_dashboard_chat_page/login_page/login_example_page.dart';
@@ -11,8 +12,8 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-
   List<SliderModal> slides = [];
+
   // List<SliderModal> slides = new List<SliderModal>();
   int currentIndex = 0;
   late PageController _controller;
@@ -36,28 +37,32 @@ class _LandingPageState extends State<LandingPage> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(child: PageView.builder(scrollDirection: Axis.horizontal,
-          onPageChanged: (value) {
-            setState(() {
-                          currentIndex = value;
-                        });
-            
-          },
-            itemCount: slides.length,
-            // itemBuilder: (BuildContext context, int index) { 
-              itemBuilder: (context, index) { 
-              return Slider(
-                image: slides[index].getImage(),
-                title: slides[index].getTitle(),
-                description: slides[index].getDescription(),
-
-              );
-             }, 
-          ),),
-           Container(
+          Expanded(
+            child: PageView.builder(
+              controller: _controller,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (value) {
+                setState(() {
+                  currentIndex = value;
+                });
+              },
+              itemCount: slides.length,
+              // itemBuilder: (BuildContext context, int index) {
+              itemBuilder: (context, index) {
+                return SliderList(
+                  image: slides[index].getImage(),
+                  title: slides[index].getTitle(),
+                  description: slides[index].getDescription(),
+                );
+              },
+            ),
+          ),
+          Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(slides.length, (index) => buildDot(index, context),
+              children: List.generate(
+                slides.length,
+                (index) => buildDot(index, context),
               ),
             ),
           ),
@@ -79,33 +84,38 @@ class _LandingPageState extends State<LandingPage> {
             margin: EdgeInsets.all(40),
             width: double.infinity,
             color: Color.fromRGBO(255, 127, 144, 1),
-   
-           // Button
-           child: FlatButton(
-             child: Text(
-                 currentIndex == slides.length ? "Mulai Sekarang": "Selanjutnya"),
-             onPressed: (){
-               if(currentIndex == slides.length){
-                 // Navigate to next screen
-                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> LoginExamplePage()),
-                 );
-               }
-               _controller.nextPage(duration: Duration(milliseconds: 100), curve: Curves.bounceIn);
-             },
-             textColor: Colors.white,
- 
-             // Border radius to button
-             shape: RoundedRectangleBorder(
-               borderRadius: BorderRadius.circular(25),
-             ),
-           ),
- 
+
+            // Button
+            child: FlatButton(
+              child: Text(currentIndex == slides.length -1
+                  ? "Mulai Sekarang"
+                  : "Selanjutnya"),
+              onPressed: () {
+                if (currentIndex == slides.length-1) {
+                  // Navigate to next screen
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginExamplePage()),
+                  );
+                }
+                _controller.nextPage(
+                    duration: Duration(milliseconds: 100),
+                    curve: Curves.bounceIn);
+              },
+              textColor: Colors.white,
+
+              // Border radius to button
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
           ),
         ],
       ),
       backgroundColor: Color.fromRGBO(255, 235, 237, 1),
     );
   }
+
   Container buildDot(int index, BuildContext context) {
     return Container(
       height: 10,
@@ -116,33 +126,7 @@ class _LandingPageState extends State<LandingPage> {
         color: Color.fromRGBO(255, 132, 148, 1),
       ),
     );
-  
   }
 }
 
 
-class Slider extends StatelessWidget {
-
-  String image;
-  String title;
-  String description;
-
-  // Slider({Key? key, required this.image, required this.title, required this.desc}) :super(key: key);
-  Slider({required this.image, required this.title, required this.description});
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Expanded(
-    child: Container(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-      Image(image: AssetImage(image)),
-      const SizedBox(height: 25),
-      Text(title, style: const TextStyle(color: Colors.black, fontSize: 28.0, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
-      Text(description, style: const TextStyle(color: Colors.black, fontSize: 18.0), textAlign: TextAlign.center),
-    ],
-    )  
-    ));
-  } 
-}
