e6263f399668ace4b889095adecb6c82a52d351b
AhmadWahyudi <ahmad.wahyudi@allianz.co.id>
2023-01-09 19:57:03 +0700

    change welcome message "tanya yuk"

--- lib/pages/chat_page/chat_patient_page/initial_consultation_load_page.dart
+++ lib/pages/chat_page/chat_patient_page/initial_consultation_load_page.dart
@@ -9,6 +9,10 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:formz/formz.dart';
 
 import '../../../data/entity/chat_message_entity.dart';
+import '../../../data/model/chat_model/chat_pending_send_request.dart';
+import '../../../data/model/hospital_model/hospital_model.dart';
+import '../../../data/model/user_model_api/user_model.dart';
+import '../../../data/shared_preference/app_shared_preference.dart';
 import '../../../utils/epragnancy_color.dart';
 import '../bloc/chat_bloc/chat_bloc.dart';
 
@@ -115,10 +119,40 @@ class _InitialConsultationLoadPageState extends State<InitialConsultationLoadPag
             });
           }
         } else if (state.type == 'fetch-active-chat-failed' && state.status != FormzStatus.submissionInProgress) {
-          Navigator.pushNamed(context, RouteName.chatPage, arguments: widget.userId).then((value) {
-            print('with data from chat page');
+          // Navigator.pushNamed(context, RouteName.chatPage, arguments: widget.userId).then((value) {
+          //   print('with data from chat page');
+          //   if(value != null){
+          //     Navigator.pop(context);
+          //   }
+          // });
+          final UserModel userModel = await AppSharedPreference.getUser();
+          final HospitalModel _hospital = await AppSharedPreference.getHospital();
+
+          ChatPendingSendRequest _chatPendingSendRequest = ChatPendingSendRequest(
+              fromId: userModel.id, hospitalId: _hospital.id, message: "Hai Bunda ${userModel.name}, selamat datang di tanya Bidan. Di sini, Bunda bisa bertanya mengenai informasi umum dan tips seputar kehamilan langsung dengan Bidan. Pertanyaan akan dijawab maksimal 3x24 jam. Yuk, Bunda bisa mulai bertanya.");
+
+          Injector.resolve<ChatBloc>().add(SendChatPendingEvent(_chatPendingSendRequest, firsTime: true));
+        } else if (state.type == 'send-pending-success' && state.chatPendingSendResponse != null) {
+          List<ChatMessageEntity> chatMessageList = [];
+          chatMessageList.add(
+              ChatMessageEntity(
+                  name: state.chatPendingSendResponse?.from?.hospital?.name,
+                  message: state.chatPendingSendResponse?.message,
+                  dateTime: state.chatPendingSendResponse?.createdDate,
+                  mine: state.chatPendingSendResponse?.fromId == widget.userId ? true: false
+              )
+          );
+          Navigator.push(context, MaterialPageRoute(builder: (context) => NewChatRoom(
+            fromId: state.chatPendingSendResponse?.fromId,
+            toImageUrl: state.chatPendingSendResponse?.to?.imageUrl,
+            toId: state.chatPendingSendResponse?.hospitalId,
+            chatMessageList: chatMessageList,
+            toName: state.chatPendingSendResponse?.to?.name ?? hospitalModel?.name ,
+            pendingChat: true,
+          ))).then((value) {
             if(value != null){
-              Navigator.pop(context);
+              print('back with data');
+              Navigator.pop(context, "back");
             }
           });
         }
@@ -165,9 +199,21 @@ class _InitialConsultationLoadPageState extends State<InitialConsultationLoadPag
     );
   }
 
+  HospitalModel? hospitalModel;
+
+  void getHospital() async {
+    HospitalModel _hospitalModel = await AppSharedPreference.getHospital();
+    if(_hospitalModel != null && mounted){
+      setState(() {
+        hospitalModel = _hospitalModel;
+      });
+    }
+  }
+
   @override
   void initState() {
     Injector.resolve<ChatBloc>().add(const FetchChatPendingPatientEvent());
+    getHospital();
     // Injector.resolve<ChatBloc>().add(const FetchChatOngoingEvent());
     super.initState();
   }
