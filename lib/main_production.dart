443cb4301b84093270de2623fd4b42920b117363
AhmadWahyudi <ahmad.wahyudi@allianz.co.id>
2023-04-12 22:26:24 +0700

    fix prod

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -95,7 +95,7 @@ Future<void> main() async {
 }
 
 final Alice aliceProd = Alice(
-    showNotification: false,
+    showNotification: true,
     darkTheme: true);
 
 class MyApp extends StatefulWidget {
